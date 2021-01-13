9868a6ae37a48314b7fd30420ef29c25307d87b0
Anton de Craen <anton.decraen@unifiedpost.com>
2021-01-13 11:00:32 +0100

    initial changes

---
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-detail-form',
+  templateUrl: './payment-detail-form.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailFormComponent implements OnInit {
+
+  constructor(public service: PaymentDetailService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    if(this.service.formData.paymentDetailId == 0) this.insertRecord(form)
+    else this.updateRecord(form);
+ }
+
+  insertRecord(form: NgForm) {
+    this.service.postPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+        // ngx-toastr component is found on from npmjs.com. Latest version is installed with 'npm i ngx-toastr'
+        // ngx-toaster depends on @angular/animations: npm install @angular/animations --save
+        // @angular/animation is already installed, see package.json
+        // add toastr css to angular.json
+        this.toastr.success("Created successfully", 'Payment Detail Register');
+      },
+      err => { console.log(err); }
+    );
+  }
+
+  updateRecord(form: NgForm) {
+    this.service.putPaymentDetail().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+        // ngx-toastr component is found on from npmjs.com. Latest version is installed with 'npm i ngx-toastr'
+        // ngx-toaster depends on @angular/animations: npm install @angular/animations --save
+        // @angular/animation is already installed, see package.json
+        // add toastr css to angular.json
+        this.toastr.info("Updated successfully", 'Payment Detail Register');
+      },
+      err => { console.log(err); }
+    );
+  }
+
+  resetForm(form: NgForm) {
+    form.form.reset();
+    this.service.formData = new PaymentDetail();
+  }
+}
---
+++ src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetail } from '../shared/payment-detail.model';
+import { PaymentDetailService } from '../shared/payment-detail.service';
+//import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor(public service: PaymentDetailService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
+
+  populateForm(paymentDetail: PaymentDetail) {
+    // need a temporary object because of two-way binding between service.formData and the html form object.
+    this.service.formData = Object.assign({}, paymentDetail);
+  }
+
+  onDelete(paymentDetail: PaymentDetail) {
+    if(confirm(`Are you sure to delete the card ${paymentDetail.cardNumber}?`)) {
+      this.service.deletePaymentDetyail(paymentDetail.paymentDetailId).subscribe(
+        res => {
+          this.service.refreshList();
+          this.toastr.error(`Card ${paymentDetail.cardNumber} deleted`, "Payment Detail Register");
+        },
+        err => { console.log(err); }
+      );
+    }
+  }
+}
