10d532a9687199eba08ef3fe1f95f23130c2d59f
Anton de Craen <anton.decraen@unifiedpost.com>
2021-01-15 00:24:53 +0100

    refactor onSubmitForm

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -16,9 +16,14 @@ export class PaymentDetailFormComponent implements OnInit {
   ngOnInit(): void { }
 
   onSubmitForm(form: NgForm): void {
-    const onSuccess = () => {
+    const onSuccess = (message: string) => {
       this.resetForm(form);
       this.service.refreshList();
+      // ngx-toastr component is found on from npmjs.com. Latest version is installed with 'npm i ngx-toastr'
+      // ngx-toaster depends on @angular/animations: npm install @angular/animations --save
+      // @angular/animation is already installed, see package.json
+      // add toastr css to angular.json
+      this.toastrService.success(message, 'Payment Detail Register');
     };
     if (this.service.formPaymentDetailId() === 0) { this.insertRecord(form, onSuccess); }
     else { this.updateRecord(form, onSuccess); }
@@ -28,30 +33,16 @@ export class PaymentDetailFormComponent implements OnInit {
     this.resetForm(form);
   }
 
-  insertRecord(form: NgForm, onSuccess: () => void): void {
+  insertRecord(form: NgForm, onSuccess: (message: string) => void): void {
     this.service.postPaymentDetail().subscribe(
-      response => {
-        onSuccess();
-        // ngx-toastr component is found on from npmjs.com. Latest version is installed with 'npm i ngx-toastr'
-        // ngx-toaster depends on @angular/animations: npm install @angular/animations --save
-        // @angular/animation is already installed, see package.json
-        // add toastr css to angular.json
-        this.toastrService.success(`Created successfully ${response.id}`, 'Payment Detail Register');
-      },
+      response => { onSuccess(`Created successfully ${response.id}`); },
       err => { console.log(err); }
     );
   }
 
-  updateRecord(form: NgForm, onSuccess: () => void): void {
+  updateRecord(form: NgForm, onSuccess: (message: string) => void): void {
     this.service.putPaymentDetail().subscribe(
-      response => {
-        onSuccess();
-        // ngx-toastr component is found on from npmjs.com. Latest version is installed with 'npm i ngx-toastr'
-        // ngx-toaster depends on @angular/animations: npm install @angular/animations --save
-        // @angular/animation is already installed, see package.json
-        // add toastr css to angular.json
-        this.toastrService.info('Updated successfully', 'Payment Detail Register');
-      },
+      response => { onSuccess('Updated successfully'); },
       err => { console.log(err); }
     );
   }
