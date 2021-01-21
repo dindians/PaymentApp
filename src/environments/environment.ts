8939476f92d1ddd06b96c6a4454112fe11f7f9cf
Anton de Craen <anton.decraen@unifiedpost.com>
2021-01-21 14:39:46 +0100

    move paymentDetailApisBaseUrl to environment settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  paymentDetailApisBaseUrl: 'https://localhost:44318/api/PaymentDetail'
 };
 
 /*
